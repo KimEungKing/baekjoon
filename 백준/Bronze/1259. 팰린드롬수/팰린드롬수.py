30949c422aebc3e60c1dd69977ac7d9a6916dfc5
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-06-04 23:20:51 +0900

    Update 팰린드롬수.py

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,12 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-# 절반으로 나눠서 앞/뒤 스트링 리버스 시켜서 비교
 while True:
     n = input().rstrip()
     if n == '0':
         break
-
+    # 뒤집어도 같으면 팰린드롬
     if n == n[::-1] :
         print('yes')
     else :
