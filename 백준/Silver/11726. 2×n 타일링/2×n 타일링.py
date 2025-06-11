ba7462e8b7b16b4e48d06eff9b8bc05ef3f19162
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-06-11 16:16:19 +0900

    Update 2×n 타일링.py

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -11,3 +11,16 @@ for i in range(4,1001):
 n = int(input())
 
 print(dp[n])
+
+# n = int(input())
+# if n == 1:
+#     print(1)
+# elif n ==2:
+#     print(2)
+# else:
+#     dp = [0] * (n+1)
+#     dp[1] = 1
+#     dp[2] = 2
+#     for i in range(3,n+1):
+#         dp[i] = (dp[i-1] + dp[i-2]) % 10007
+#     print(dp[n])
