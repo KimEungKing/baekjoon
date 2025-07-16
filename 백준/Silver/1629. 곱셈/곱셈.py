13e7a2f3b2ccd9371d758d4f50301e3d52d2ab34
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-07-16 09:53:04 +0900

    [Silver I] Title: 곱셈, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+# print(a**b%c) # 초과
+# print(pow(a,b,c))
+
+# 제곱 곱
+arr = bin(b)
+arr = list(arr[2:])
+arr.reverse()
+answer = 1
+current = a % c
+
+for i, n in enumerate(arr,start=1):
+    if n == '1':
+        answer = answer * current % c
+    current = current * current % c
+print(answer)
