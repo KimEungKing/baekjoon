26aa71f97e1dd155782f77b208ef77db19cd9e2a
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-07-25 09:00:18 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 952 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+s = input().rstrip()
+answer = ''
+stk = []
+judge = False
+
+# 괄호열렸으면 닫힐때까지 answer에 추가
+for i in s:
+
+    if i == '<':
+        judge = True
+        for _ in range(len(stk)):
+            answer += stk.pop()
+        stk.append(i)
+
+    elif i == '>':
+        judge = False
+        stk.append(i)
+        for _ in range(len(stk)):
+            answer += stk.pop(0)\
+
+    elif i == ' ' and not judge:
+        for _ in range(len(stk)):
+            answer += stk.pop()
+        answer += ' '
+    else:
+        stk.append(i)
+if stk:
+    for _ in range(len(stk)):
+        answer += stk.pop()
+
+print(answer)
