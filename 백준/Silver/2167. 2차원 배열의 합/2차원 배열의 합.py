78a72c0c1333e07e719d45ff0670e34f1084be80
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-07-29 09:20:32 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 116 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,15 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(n)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 k = int(input())
+preSum = [[0]*(m+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        preSum[i][j] = arr[i-1][j-1] + preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1]
 
+answer = 0
 for _ in range(k):
-    i,j,x,y = map(int,input().split())
+    i, j, x, y = map(int, input().split())
 
-    answer = 0
-    for t in range(i-1,x):
-        answer += sum(arr[t][j-1:y])
-        
+    answer = preSum[x][y] - preSum[i-1][y] - preSum[x][j-1] + preSum[i-1][j-1]
+
     print(answer)
