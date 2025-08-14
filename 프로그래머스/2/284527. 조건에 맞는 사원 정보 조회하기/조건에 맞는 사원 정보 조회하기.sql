df8705a767a97d745315eb138dbf9e6d23a6cb8d
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-08-14 10:24:51 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,7 @@
+select (sum(score)) as score, e.emp_no,
+    e.emp_name, e.position, e.email
+from hr_employees as e 
+join hr_grade as g on e.emp_no = g.emp_no
+group by g.emp_no 
+order by 1 desc 
+limit 1 
