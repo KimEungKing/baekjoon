f2c59384786d5268f0f6e581a1969addca8c7e20
kimeungjun <113088540+KimEungKing@users.noreply.github.com>
2025-09-15 14:27:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.41 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,20 +1,27 @@
 def solution(left, right):
-    arr = [i for i in range(left,right+1)]
+    # arr = [i for i in range(left,right+1)]
     answer = 0 
-    for i in arr: 
-        cnt = 0
-        # 약수 개수 구하기 
-        for n in range(1,int(i**0.5) + 1):
-            if i % n == 0:
-                if not n*n == i:
-                    cnt += 2
-                else:
-                    cnt += 1
-                    
-        if cnt % 2 == 0 :
-            answer += i
-        else:
+    for i in range(left,right+1): 
+        if int(i**0.5) == i**0.5: # odd
             answer -= i
+        else:
+            answer += i
+    return answer
             
-    return answer 
+        
+        
+    #### 약수를 구할 필요가 없음. 약수가 짝수인지 홀수인지만 판별하면 됨 
+#         # 약수 개수 구하기 
+#         for n in range(1,int(i**0.5) + 1):
+#             if i % n == 0:
+#                 if not n*n == i:
+#                     cnt += 2
+#                 else:
+#                     cnt += 1
+                    
+#         if cnt % 2 == 0 :
+#             answer += i
+#         else:
+#             answer -= i
+            
                     
